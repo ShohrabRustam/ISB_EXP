d4e18bcd2ce2a80902d5e00bf7789f85d40ed68d
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-04-18 15:27:54 +0530

    setup the brach2

---
+++ resources/views/Layout/section.blade.php
@@ -0,0 +1,10 @@
+<section class="headline">
+    <div class="bg"></div>
+    <div class="bg bg2"></div>
+    <div class="bg bg3"></div>
+
+    <h1>Insurance  Broking System</h1>
+    <p class="headPara">“Term life insurance is a good defensive game plan”</p>
+    <p class="headPara">Life insurance offers you Long-term Savings which will give huge benefit later, feel allowed to make inquiry.</p>
+    <p class="headPara">I don’t call it “Life Insurance,” I call it “Love Insurance.” We buy it because we want to leave a legacy for those we love.</p>
+  </section>
---
+++ resources/views/Users/about.blade.php
@@ -0,0 +1,8 @@
+@extends('Layout.master')
+@section('title')
+About Us
+@endsection
+
+@section('section')
+<h1>About Page</h1>
+@endsection
